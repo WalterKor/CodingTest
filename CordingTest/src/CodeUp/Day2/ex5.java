b0f618b7330b9a102df5f895d356b2e95d0726f6
everglow <78077569+okchanho@users.noreply.github.com>
2021-04-23 23:20:08 +0900

    1일백준

---
+++ CordingTest/src/CodeUp/Day2/ex5.java
@@ -0,0 +1,16 @@
+package CodeUp.Day2;
+
+public class ex5 {
+	public static void main(String[] args) {
+		/*
+		 첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+		 예제) 1 2
+		 예제) 3
+		 */
+		
+		int a = 1;
+		int b = 2;
+		System.out.printf("%d + %d = %d",a,b, (a+b));
+		
+	}
+}
