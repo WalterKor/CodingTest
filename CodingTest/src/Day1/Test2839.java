74cd9f2987190839ef10cd60612f8a4d065e0873
everglow <78077569+okchanho@users.noreply.github.com>
2021-08-30 20:20:07 +0900

    8/30
    8/30

---
+++ CodingTest/src/Day1/Test2839.java
@@ -0,0 +1,7 @@
+package Day1;
+
+public class Test2839 {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
---
+++ CordingTest/src/Day6/Test2839.java
@@ -0,0 +1,9 @@
+package Day6;
+
+public class Test2839 {
+    public static void main(String[] args) {
+
+        System.out.println("he;;p");
+
+    }
+}
